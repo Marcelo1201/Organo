49d082a828918b45d718d671089d48588b636eb2
Marcelo Giacometti <marcelo.giacomettigemeo1@gmail.com>
2022-10-07 20:39:09 -0300

    Starting the header

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner(){
+    return(
+        <header className="banner">
+            <img src="/img/banner.jpg" alt="Banner principal da página do organo"/>
+        </header>
+    )
+}
+
+export default Banner;
